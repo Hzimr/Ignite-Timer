bb6de41a9c03d98a145f9d605cfba5c5eb5c8215
Hítalo Rodrigues <hitalo005@gmail.com>
2024-04-10 09:24:39 -0300

    feat: created button example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from "./components/button/button"
 import { Header } from "./components/header/header"
 
 export function App() {
   return (
       <div>
         <Header />
+        <Button variant='danger' />
+        <Button variant="secondary"/>
+        <Button variant="success"/>
+        <Button variant="danger"/>
         <h1>Bom dia</h1>
       </div>
   )
