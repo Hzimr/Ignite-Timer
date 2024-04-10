4377a067227b818fd0101fa9bc76d4bf7854aeef
Hítalo Rodrigues <hitalo005@gmail.com>
2024-04-10 11:56:16 -0300

    styles: config themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Button } from "./components/button/button"
 import { Header } from "./components/header/header"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/defaults"
 
 export function App() {
   return (
+    <ThemeProvider theme={defaultTheme}>
       <div>
         <Header />
         <Button variant='danger' />
@@ -11,6 +14,7 @@ export function App() {
         <Button variant="danger"/>
         <h1>Bom dia</h1>
       </div>
+    </ThemeProvider>
   )
 }
 
