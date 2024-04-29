1490694a0452eb4840c6c90bff60d732ceb44f80
Hítalo Rodrigues <hitalo005@gmail.com>
2024-04-29 19:32:16 -0300

    feat: created Routers in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import { Button } from './components/button/button'
 import { Header } from './components/header/header'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/defaults'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <div>
-        <Header />
-        <Button variant="danger" />
-        <Button variant="secondary" />
-        <Button variant="success" />
-        <Button variant="danger" />
-        <h1>Bom dia</h1>
-      </div>
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <div>
+          <Header />
+          <Router />
+        </div>
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
