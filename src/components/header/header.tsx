d93ffc78eef204b86a786c5a6879a5137267b7d5
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-01 10:07:44 -0300

    feat: created stylesHeader

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,24 @@
+import { HeaderContainer } from './stylesHeader'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import menuHistory from '../../assets/menuHistory.png'
+import menuTime from '../../assets/menuTime.png'
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <HeaderContainer>
+      <img
+        src={logoIgnite}
+        alt="Dois triângulos da cor verde com uma inclinação de 45º graus "
+      />
+      <nav>
+        <a href="">
+          <img src={menuTime} alt="" />
+        </a>
+        <a href="">
+          <img src={menuHistory} alt="" />
+        </a>
+      </nav>
+    </HeaderContainer>
   )
 }
