359d7855e56ec83ad47ccb542ffc2cb7d8ba8dc0
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-06 17:05:55 -0300

    feat: created typescript in the form

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -12,6 +12,11 @@ import {
   TaskInput,
 } from './stylesHome'
 
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
@@ -21,12 +26,18 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 5,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
