ded512a853a210c764acbb5e13cc2521b2c0a3c2
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-07 13:34:39 -0300

    refactor: added newCycleSchema

--- src/pages/home/components/newCycleForm/newCycleForm.tsx
+++ src/pages/home/components/newCycleForm/newCycleForm.tsx
@@ -1,6 +1,28 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './newCycleForm'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .int()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function NewCycleForm() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 5,
+    },
+  })
+
   return (
     <FormContainer>
       <label htmlFor="task">Vou trabalhar em</label>
