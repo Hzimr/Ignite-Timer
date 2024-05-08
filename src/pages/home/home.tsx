a9fd2a505ca8eb0b54146f9e93e44c145c7dbf96
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-08 12:30:13 -0300

    refactor: created setSecondsPassed and added in CyclesContext

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -24,7 +24,9 @@ interface Cycle {
 interface CyclesContextType {
   activeCycle: Cycle | undefined
   activeCycleId: string | null
+  amountSecondsPassed: number
   markCurrentCycleAsFinished: () => void
+  setSecondsPassed: (seconds: number) => void
 }
 
 export const CyclesContext = createContext({} as CyclesContextType)
@@ -44,6 +46,8 @@ export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
 
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
   const newCycleForm = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -55,6 +59,10 @@ export function Home() {
   const { handleSubmit, watch, reset } = newCycleForm
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
 
   function markCurrentCycleAsFinished() {
     setCycles((state) =>
@@ -101,20 +109,19 @@ export function Home() {
   const task = watch('task')
   const isSubmitDisabled = !task
 
-  /** Prop Drilling => Quando a gente tem MUITAS
-   *  propriedades APENAS para comunicação entre
-   *  componentes
-   *  Context API => Permite Compartilharmos informações
-   *  entre VÁRIOS componentes ao mesmo tempo
-   * */
-
   console.log(cycles)
 
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <CyclesContext.Provider
-          value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}
+          value={{
+            activeCycle,
+            activeCycleId,
+            markCurrentCycleAsFinished,
+            amountSecondsPassed,
+            setSecondsPassed,
+          }}
         >
           <FormProvider {...newCycleForm}>
             <NewCycleForm />
